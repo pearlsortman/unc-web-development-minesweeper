dea678833e6f7501575264fe678dd6177919084e
PearlSortman <sortman@live.unc.edu>
2014-10-07 21:46:45 -0400

    New version

--- minesweeper.js
+++ minesweeper.js
@@ -1,17 +1,18 @@
 // testInput function ensures all numbers input by the user are valid for the game
-// grid must be between 10x10 and 40x30
+// grid must be between 8x8 and 40x30
 // max mines must be less than columns*rows
-// collaborated with Faith Walker
-function testInput()
-{
+
+function testInput() { // function to validate user inputs
+	alert("working...");
 	var columns = document.getElementById("x-axis").value;
 	var rows = document.getElementById("y-axis").value;
 	var mines = document.getElementById("mines").value;
+	
 
-	if(columns<8 || rows<8 || columns>40 || rows>30) {
+	if(columns<8 || rows<8 || columns>40 || rows>30) { // test that grids are within bounds
 		alert("Sorry, please enter a grid size between 8x8 and 40x30.");
 	}
-	else if (mines>=(columns*rows)) {
+	else if (mines>=(columns*rows)) { // test that the number of mines does not exceed the grid area
 		alert("Sorry, please enter a number of mines less than " + (columns*rows));
 	}
 	else {
@@ -20,19 +21,19 @@ function testInput()
 		+ " by "
 		+ document.getElementById("y-axis").value;
 		alert(popup_message);
-	}
 
-	createGrid(columns, rows, mines);
-	setMines(columns, rows, mines);
+		createGrid(columns, rows, mines);
+	}
 }
 
-function createGrid(columns, rows, mines) {
+function createGrid(columns, rows, mines) { // function to create the grid area
+	alert("activation complete... all systems go!");
 	var $newRow;
+	var $myGrid;
 	var cell;
-	var i, j, k;
-	var cellID=1;
-	var gridArea = columns*rows;
-	var setDefaultZero = 1;
+	var i, j;
+
+	$('table').html(""); // this line will remove a previous grid when creating a new one
 
 	for (i=0; i<rows; i++) {
     	$newRow = $('<tr></tr>');
@@ -40,152 +41,100 @@ function createGrid(columns, rows, mines) {
     	for (j=0; j<columns; j++) {
     		cell = document.createElement("td");
         	$newRow.append(cell); // Append an empty <td> element to the row that we are building.
-    		cell.setAttribute("id", cellID);
+    		cell.setAttribute("id", i + "_" + j)
     		//cell.setAttribute("class", "unclicked");
-    		cell.setAttribute("class", "notMine");
-    		cellID++;
+    		cell.setAttribute("class", 0);
+
+    		$('table').append($newRow);
     	}
-
-    	$('table').append($newRow);
+    	alert("row added");
 	}
 
-	for (k=1; k<gridArea+1; k++) 
-	{
-		setDefaultZero = $('table').find('#'+k);
-		setDefaultZero.text("0");
-	}
-
+	generate(columns, rows, mines);
 }
 
-function setMines(columns, rows, mines) 
-{
-	var mineLocation;
-	var whereDaMinesAt = [];
-	var mineCounter = 0;
-	//this.columns = columns;
-	//this.rows = rows;
-	//this.mines = mines;
-	var gridArea = columns*rows;
+function generate(columns, rows, mines) { // function to generate bombs
+	var xLocation;
+	var yLocation;
+	var i;
+	var cell;
 
-	while (mineCounter<mines) 
-	{
-		mineLocation = (Math.floor(Math.random() * gridArea));
-		cell = $('table').find('#'+mineLocation);
-
-		if (cell.hasClass("notMine")) 
-		{
-			cell.attr("class", "isMine");
-			cell.text("*");
-			mineCounter++;
-			//cell.attr("src", "bomb.jpg");
-			whereDaMinesAt.push( mineLocation );
-			alert("MineLocation at time of placement = " + whereDaMinesAt[0]);
+	for (i=0; i<mines; i++) {
+		xLocation = (Math.floor(Math.random() * rows));
+		yLocation = (Math.floor(Math.random() * columns));
+		cell = "#" + xLocation + "_" + yLocation;
+		if ($(cell).hasClass("9")) {
+			i--;
 		}
-		else 
-		{
-			//proceed through while loop
-		}	
-	}	
-
-	alert("mineCounter after all placed = " + mineCounter);
-
-	var n;
-	var numMinesNear=0;
-	var currentCellID=0;
-	var cellToPopulate;
-
-	for (n=0; n<mineCounter; n++)
-	{
-		var adjCells = [];
-		alert("whereDaMinesAt[n] = " + n + whereDaMinesAt[n]);
-		var currentMine = whereDaMinesAt[n];
-		alert("Processing mine at location = " + currentMine);
-		
-		//if mine is top left corner
-		if (currentMine===1)
-		{
-			alert("Mine is in top left corner.");
-			adjCells.push( (currentMine+1), (currentMine+columns), (currentMine+columns+1) );
+		else {
+			$(cell).attr("class", "9");
 		}
-
-		//if mine is top right corner
-		else if (currentMine===columns)
-		{
-			alert("Mine is in top right corner.");
-			adjCells.push( (currentMine-1), (currentMine+columns-1), (currentMine+columns) );
-		}
-
-		//mine bottom left corner
-		else if (currentMine===gridArea-columns)
-		{
-			alert("Mine is in bottom left corner.");
-			adjCells.push( (currentMine+1), (currentMine-columns), (currentMine-columns-1) );
-		}
-
-		//mine bottom right corner
-		else if (currentMine===gridArea)
-		{
-			alert("Mine is in bottom right corner.");
-			adjCells.push( (currentMine-1), (currentMine-columns), (currentMine-columns-1) );
-		}
-
-		//if mine is on top edge
-		else if (currentMine<=columns) 
-		{
-			alert("Mine is on top edge.");
-			adjCells.push( (currentMine-1), (currentMine+1), (currentMine+columns-1), (currentMine+columns), (currentMine+columns+1) );
-		}
-
-		//if mine is on bottom edge
-		else if (currentMine>=(gridArea-columns)) 
-		{
-			alert("Mine is on bottom edge.");
-			adjCells.push( (currentMine-1), (currentMine+1), (currentMine-columns-1), (currentMine-columns), (currentMine-columns-1) );
-		}
-
-		//if mine is on right edge
-		else if (((currentMine+1)%columns)===0)
-		{
-			alert("Mine is on right edge.");
-			adjCells.push( (currentMine-1), (currentMine-columns-1), (currentMine-columns), (currentMine+columns-1), (currentMine-columns) );
-		}
-
-		//if mine is on left edge
-		else if (currentMine%columns) 
-		{
-			alert("Mine is on left edge.");
-			adjCells.push( (currentMine-columns), (currentMine-columns+1), (currentMine+1), (currentMine+columns), (currentMine+columns+1) );
-		}
-
-		else //centrally located
-		{
-			alert("Mine is in center.");
-			adjCells.push( (currentMine-1), (currentMine+1), (currentMine-columns-1), (currentMine-columns), (currentMine-columns+1), (currentMine+columns-1), (currentMine+columns), (currentMine+columns+1) );
-		}
-
-		for (j=0; j<adjCells.length-1; j++) 
-		{
-
-			currentCellID = adjCells[j];
-			cellToPopulate = $('table').find('#'+currentCellID);
-			alert("currentCellID = " + currentCellID);
-			alert("adjCells[j] = " + adjCells[j]);
-
-			if (cellToPopulate.hasClass("notMine")) 
-			{
-				alert("Cell is not a mine, popluating number...");
-				numMinesNear = parseInt(cellToPopulate.text());
-				numMinesNear++;
-				cellToPopulate.text(numMinesNear);
-			}
-			else 
-			{
-				alert("Cell is a mine, will not populate with number.");
-				//don't assign number to mine
-			}
-
-		}
-
 	}
 
+	bombCount(columns, rows);
+}
+
+function bombCount(columns, rows) { // function to generate numbers on non-mine squares
+	var i, j;
+	var cell;
+	var bombCounter;
+	var top_left;
+	var top_center;
+	var top_right;
+	var left;
+	var right;
+	var bot_left;
+	var bot_center;
+	var bot_right;
+
+	for (i=0; i<rows; i++) {
+		for (j=0; j<columns; j++) {
+
+			cell = "#" + i + "_" + j;
+			top_left = "#" + (i-1) + "_" + (j-1);
+			top_center = "#" + (i-1) + "_" + j;
+			top_right = "#" + (i-1) + "_" + (j+1);
+			left = "#" + i + "_" + (j-1);
+			right = "#" + i + "_" + (j+1);
+			bot_left = "#" + (i+1) + "_" + (j-1);
+			bot_center = "#" + (i+1) + "_" + j;
+			bot_right = "#" + (i+1) + "_" + (j+1);
+
+			bombCounter = 0;
+
+			if (!($(cell).hasClass("9"))) {
+
+				if ($(top_left).hasClass("9")) {
+					bombCounter++;
+				}
+				if ($(top_center).hasClass("9")) {
+					bombCounter++;
+				}
+				if ($(top_right).hasClass("9")) {
+					bombCounter++;
+				}
+				if ($(left).hasClass("9")) {
+					bombCounter++;
+				}
+				if ($(right).hasClass("9")) {
+					bombCounter++;
+				}
+				if ($(bot_left).hasClass("9")) {
+					bombCounter++;
+				}
+				if ($(bot_center).hasClass("9")) {
+					bombCounter++;
+				}
+				if ($(bot_right).hasClass("9")) {
+					bombCounter++;
+				}
+
+				$(cell).attr("class", bombCounter);
+
+				if (!(bombCounter == 0)) { // only if there is an adjacent mine do we display a number
+					$(cell).html(bombCounter);
+				}
+			}
+		}
+	}
 }
